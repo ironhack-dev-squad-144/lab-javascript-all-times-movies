f1f56077644b6c105a17637ce09e363cb23fc1a0
Maxence Bouret <maxence.bouret@gmail.com>
2019-06-06 12:57:58 +0100

    Iteration 1

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,6 +1,33 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
+function turnHoursToMinutes(movies) {
+  return movies.map(movie => {
+    let strDuration = movie.duration
+    let duration // We need to 
 
+    // When strDuration === "Xh XXmin"
+    if (strDuration.length === "Xh XXmin".length)
+      duration = strDuration[0]*60 + strDuration[3]*10 + strDuration[4]*1
+    // When strDuration === "Xh Xmin"
+    if (strDuration.length === "Xh Xmin".length)
+      duration = strDuration[0]*60 + strDuration[3]*1
+    // When strDuration === "XXmin"
+    if (strDuration.length === "XXmin".length)
+      duration = strDuration[0]*10 + strDuration[1]*1
+    // When strDuration === "Xh"
+    if (strDuration.length === "Xh".length)
+      duration = strDuration[0]*60
+
+    return {
+      title: movie.title,
+      year: movie.year,
+      director: movie.director,
+      duration: duration,
+      genre: movie.genre,
+      rate: movie.rate
+    }
+  })
+}
 
 // Get the average of all rates with 2 decimals 
 
